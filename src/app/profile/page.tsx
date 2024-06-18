84ab2374c8345a3ac719187ffb772c2cca532941
VadymPertsov <v.pertsov@quantumobile.com>
2024-06-18 15:01:31 +0300

    fix: refactoring ProfilePage

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,20 @@
-import { ProfileClips } from '@/modules/profile'
+'use client'
+
+import { useSelectedClipsContext } from '@/context/SelectedClipsContext'
+import { ClipList } from '@/modules/clips'
 
 const ProfilePage = () => {
-  return <ProfileClips />
+  const { selectedClips: clipsData } = useSelectedClipsContext()
+
+  return (
+    <>
+      <ClipList
+        clipsList={clipsData}
+        isLoading={false}
+        title="Watch the clips and enjoy :)"
+      />
+    </>
+  )
 }
 
 export default ProfilePage
