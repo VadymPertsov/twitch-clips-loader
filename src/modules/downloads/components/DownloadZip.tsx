8d94e7dc0c8e3f66252b18b575b4fecf56da2c1f
VadymPertsov <v.pertsov@quantumobile.com>
2024-06-20 13:00:50 +0300

    feat: add react-query to DownloadZip component

--- src/modules/downloads/components/DownloadZip.tsx
+++ src/modules/downloads/components/DownloadZip.tsx
@@ -4,7 +4,8 @@ import ActionTag from '@/components/ui/ActionTag'
 import { Clip } from '@/types'
 import { normalizeUrltoMp4 } from '@/utils/helpers-functions'
 import axios from 'axios'
-import { useState } from 'react'
+import { useMemo } from 'react'
+import { useMutation } from 'react-query'
 
 interface DownloadZipProps {
   clipsList: Clip[]
@@ -13,13 +14,13 @@ interface DownloadZipProps {
 const DownloadZip = (props: DownloadZipProps) => {
   const { clipsList } = props
 
-  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const urls = useMemo(
+    () => clipsList.map(clip => normalizeUrltoMp4(clip.thumbnail_url)),
+    [clipsList]
+  )
 
-  const urls = clipsList.map(clip => normalizeUrltoMp4(clip.thumbnail_url))
-
-  const downloadAllFiles = async () => {
-    setIsLoading(true)
-    try {
+  const { mutate: downloadAllFiles, isLoading } = useMutation(
+    async () => {
       const response = await axios.post(
         '/api/archive',
         { urls },
@@ -30,23 +31,32 @@ const DownloadZip = (props: DownloadZipProps) => {
         throw new Error('Failed to download files')
       }
 
-      const url = window.URL.createObjectURL(new Blob([response.data]))
-      const a = document.createElement('a')
-      a.href = url
-      a.download = 'files.zip'
-      document.body.appendChild(a)
-      a.click()
-      a.remove()
-    } catch (error) {
-      console.error('Error downloading files:', error)
-    } finally {
-      setIsLoading(false)
+      return response.data
+    },
+    {
+      onSuccess: data => {
+        const url = window.URL.createObjectURL(new Blob([data]))
+        const a = document.createElement('a')
+        a.href = url
+        a.download = 'files.zip'
+        document.body.appendChild(a)
+        a.click()
+        a.remove()
+      },
+      onError: error => {
+        console.error('Error downloading files:', error)
+      },
     }
-  }
+  )
 
   return (
     <Container className="flex w-full flex-col items-center justify-center gap-3 pt-10">
-      <ActionTag as="button" onClick={downloadAllFiles}>
+      <ActionTag
+        disabled={isLoading}
+        as="button"
+        onClick={() => downloadAllFiles()}
+        title="If the download takes more than 1 minute, you will receive an error and a damaged archive. Try selecting fewer clips. A fix for this issue is under development."
+      >
         {isLoading ? <Loader /> : 'Download All in ZIP'}
       </ActionTag>
       {isLoading && <p>Please wait a bit...</p>}
